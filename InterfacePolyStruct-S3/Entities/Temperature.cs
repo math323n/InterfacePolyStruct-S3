5a4f5db373195bd12a3aee87a1e0d0f743bf77f6
Troll Coder <math323n@edu.campusvejle.dk>
2020-08-14 10:37:26 +0200

    Added more

---
+++ InterfacePolyStruct-S3/Entities/Temperature.cs
@@ -0,0 +1,105 @@
+﻿using System;
+
+
+
+
+namespace InterfacePolyStruct_S3.Entities
+{
+    public class Temperature: Object, IEquatable<object>, IComparable, ICloneable
+    {
+
+        protected double celsius;
+        protected double fahrenheit;
+        protected string text;
+
+        public Temperature(double celsius, double fahrenheit, string text)
+        {
+            Celsius = celsius;
+            Fahrenheit = fahrenheit;
+            Text = text;
+        }
+
+        public double Celsius
+        {
+            get
+            {
+                return celsius;
+            }
+
+            set
+            {
+                celsius = value;
+            }
+        }
+
+        public double Fahrenheit
+        {
+            get
+            {
+                return fahrenheit;
+            }
+
+            set
+            {
+                fahrenheit = value;
+            }
+        }
+
+        public string Text
+        {
+            get
+            {
+                return text;
+            }
+
+            set
+            {
+                text = value;
+            }
+        }
+
+        #region Methods
+        /// <summary>
+        /// Clone method from  ICloneable
+        /// </summary>
+        /// <returns></returns>
+        public object Clone()
+        {
+            object clone = MemberwiseClone();
+
+            return clone;
+        }
+
+        /// <summary>
+        /// CompareTo from IComparable
+        /// </summary>
+        /// <param name="other"></param>
+        /// <returns></returns>
+        public int CompareTo(object other)
+        {
+            if(other == null)
+            {
+                return 1;
+            }
+            // Cast other as Temperature
+
+            // if NOT null return the CompareTo
+            if(other is Temperature otherTemperature)
+            {
+                return celsius.CompareTo(otherTemperature.celsius);
+            }
+            // Else throw ArgumentException
+            else
+            {
+                throw new ArgumentException("Object is not a Temperature");
+            }
+
+        }
+
+        public bool Equals( Temperature other)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+    }
+}
--- InterfacePolyStruct-S3/Program.cs
+++ InterfacePolyStruct-S3/Program.cs
@@ -1,5 +1,7 @@
 ﻿
+using InterfacePolyStruct_S3.Entities;
 using System;
+using System.Collections;
 
 namespace InterfacePolyStruct_S3
 {
@@ -7,7 +9,34 @@ namespace InterfacePolyStruct_S3
     {
         static void Main()
         {
-           // Start
+            ArrayList temperatures = new ArrayList();
+            // Initialize random number generator.
+            Random rnd = new Random();
+
+            // Generate 10 temperatures between 0 and 100 randomly.
+            for(int ctr = 1; ctr <= 10; ctr++)
+            {
+                int celsius = rnd.Next(0, 100);
+                int fahrenheit = rnd.Next(0, 456);
+
+                Temperature temp = new Temperature(celsius, fahrenheit, "test");
+   
+                temperatures.Add(temp);
+            }
+
+            // Sort ArrayList.
+            temperatures.Sort();
+
+            foreach(Temperature temp in temperatures)
+                Console.WriteLine(temp.Celsius);
+
+            Temperature temptest = new Temperature(18, 23, "test");
+            Temperature otherTest = new Temperature(19, 24, "test");
+            Console.WriteLine(temptest.CompareTo(otherTest));
+
+            Temperature cloneTest = (Temperature)temptest.Clone();
+            Console.WriteLine(temptest.Celsius);
+            Console.WriteLine(cloneTest.Celsius);
         }
     }
 }
